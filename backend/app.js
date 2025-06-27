eabb5b220c733db86fa4ea63515300aa52554fda
PumpkinManiac <peeyush.verma10@gmail.com>
2025-06-27 21:15:01 +0530

    d

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 
 
 const app = express();
-app.use(cors({origin:"http://localhost:5173", credentials: true}))
+app.use(cors({origin:"http://final-chatbot-nine.vercel.app", credentials: true}))
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
